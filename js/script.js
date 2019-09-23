9e5f3c845b8e7a00f1c5ecaeacf28d121dc5953f
Rabbit <anastasiar.tishkina@gmail.com>
2019-09-23 20:30:50 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+// Go to top of page
+(function() {
+
+  'use strict';
+
+  if (!('querySelector' in document && 'addEventListener' in window && Array.prototype.forEach)) {
+    return;
+  }
+  // Function to animate the scroll
+  var smoothScroll = function (anchor, duration) {
+
+    // Calculate how far and how fast to scroll
+    var startLocation = window.pageYOffset;
+    var endLocation = anchor.offsetTop;
+    var distance = endLocation - startLocation;
+    var increments = distance/(duration/64);
+
+    // Scroll the page by an increment, and check if it's time to stop
+    var animateScroll = function () {
+      window.scrollBy(0, increments);
+      stopAnimation();
+    };
+
+    // If scrolling down
+    if ( increments >= 0 ) {
+      // Stop animation when you reach the anchor OR the bottom of the page
+      var stopAnimation = function () {
+        var travelled = window.pageYOffset;
+        if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
+          clearInterval(runAnimation);
+        }
+      };
+    }
+    // If scrolling up
+    else {
+      // Stop animation when you reach the anchor OR the top of the page
+      var stopAnimation = function () {
+        var travelled = window.pageYOffset;
+        if ( travelled <= (endLocation || 0) ) {
+          clearInterval(runAnimation);
+        }
+      };
+    }
+    // Loop the animation function
+    var runAnimation = setInterval(animateScroll, 16);
+  };
+
+  // Define smooth scroll links
+  var scrollToggle = document.querySelectorAll('.js-scroll');
+
+  // For each smooth scroll link
+  Array.prototype.forEach.call(scrollToggle, function (toggle) {
+
+    // When the smooth scroll link is clicked
+    toggle.addEventListener('click', function(e) {
+      e.preventDefault();
+      // Get anchor link and calculate distance from the top
+      var dataID = toggle.getAttribute('scroll-anchor');
+      var dataTarget = document.querySelector(dataID);
+      // If the anchor exists
+      if (dataTarget) {
+          // Scroll to the anchor
+          smoothScroll(dataTarget, 800);
+      }
+    }, false);
+  });
+
+ })();
+
+
